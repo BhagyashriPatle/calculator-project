f216be0f3096eb861dd74f8db9bc32847a73b25b
Rakesh Nekenti <monapatle1969@gmail.com>
2020-08-27 22:36:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+python program for simple calculator using class function
+'''
+
+class calculator:
+    """
+    This class holds operations to perform arithmetic calculations
+
+    instance method:
+
+    """
+
+# function to addition two numbers
+    def add(num1,num2):
+        return num1+num2
+
+# function to subtract two numbers
+    def subtract(num1,num2):
+        return num1-num2
+
+# function to multiply two numbers
+    def multiply(num1,num2):
+        return num1*num2
+
+# function to divide two numbers
+    def divide(num1,num2):
+        return num1/num2
+
